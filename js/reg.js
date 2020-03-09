1214b66f06645a89d55b94544cad5fbdcb79b4b7
wowoliu <lhh1748089493@163.com>
2020-03-09 17:32:20 +0800

    test

--- js/reg.js
+++ js/reg.js
@@ -1,1 +1,30 @@
-let a = 10;
+$(function(){
+    // $("#username").change(function(){
+    //     $.get("http://jx.xuzhixiang.top/ap/api/checkname.php",
+    //      {username: $("#username").val()}, data =>{
+    //          if(data.code == 1){
+    //              $()
+    //          }
+    //      });
+    // });
+    $("#regbtn").click(function(){
+        $.get("http://jx.xuzhixiang.top/ap/api/checkname.php",
+         {username: $("#username").val()}, data =>{
+             if(data.code == 1){
+                 //注册接口
+                 $.get("http://jx.xuzhixiang.top/ap/api/reg.php",
+                 {
+                     username: $("#username").val(),
+                     password :$("#psw").val()
+                 },data => {
+                     if(data.code == 1){
+                         //注册成功跳转到登陆页面
+                         location.href = "login.html";
+                     }
+                 })
+             } else{
+                 alert("用户名重名重新输入")
+             }
+         });
+    })
+});
